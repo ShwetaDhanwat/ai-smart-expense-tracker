ae51f0599acad2d953b0c68feae023771726920a
Shweta Dhanwat <shwetadhanwat@gmail.com>
2026-07-14 14:44:28 +0530

    Fix duplicate ThemeProvider import

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { ExpenseProvider } from "./context/ExpenseContext";
-import { ThemeProvider } from "./context/ThemeContext";
+
 
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
